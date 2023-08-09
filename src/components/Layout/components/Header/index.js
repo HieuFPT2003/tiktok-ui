7402dccd9ccab2b0f7aec7cd42a9ccf4f2712fad
Andree Nguyen <hieufpt2003@gmail.com>
2023-08-09 15:19:29 +0700

    add logo

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,16 +1,24 @@
-
-import classNames from "classnames/bind";
+import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            {/*Logo*/}
-            {/*Search*/}
-        </div>
-    </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="logo-tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button>{/** Clear */}</button>
+                    {/** Loading */}
+                    <button className={cx('search-btn')}>{/** */}</button>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
